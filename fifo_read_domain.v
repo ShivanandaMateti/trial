4a23aa0c76194ec9b6dda5fb703a71a92d3e2881
ShivanandaMateti <shivamateti60@gmail.com>
2026-06-07 18:52:39 +0530

    update read domain

--- fifo_read_domain.v
+++ fifo_read_domain.v
@@ -5,14 +5,16 @@ module fifo_read_domain(
                             input [Addresswidth:0] write_ptr_grey_sync,
                             input read_clk,
                             input read_reset,
+                            input read_enable,
                             output [Addresswidth-1:0] read_address,
                             output [Addresswidth:0]   read_ptr_grey,
-                            output empty,
-
+                            output empty
 )
 
-wire read_ptr;
-reg read_address_reg;
+wire [Addresswidth:0]read_ptr;
+reg [Addresswidth:0]read_address_reg;
+wire [Addresswidth:0]read_ptr_next;
+wire [Addresswidth:0]read_ptr_grey_next;
 
 always@(posedge read_clk,posedge read_reset)
 begin
@@ -21,10 +23,10 @@ begin
         read_ptr <= 0;
         read_address_reg <= 0;
     end
-    else
+    else if (read_enable && !empty)
     begin
-        read_ptr = read_ptr + 1;
-        read_address_reg = read_ptr[Addresswidth-1:0];
+        read_ptr <= read_ptr + 1;
+        read_address_reg <= read_ptr[Addresswidth-1:0];
     end
 end
 
@@ -32,8 +34,10 @@ assign read_address = read_address_reg;
 assign read_ptr_next = read_ptr + 1;
 
 B_G  R1(read_ptr,read_ptr_grey);
+B_G  R1(read_ptr_next,read_ptr_grey_next);
 
-assign empty = (read_ptr_grey  == write_ptr_grey_sync);
+
+assign empty = (read_ptr_grey_next  == write_ptr_grey_sync);
 
 endmodule
 
@@ -41,14 +45,6 @@ endmodule
 
 
 module B_G(input [Addresswidth:0]binary,output [ Addresswidth:0]grey);
-genvar i;
-generate for(i=0;i<Addresswidth+1;i=i+1)
-        begin : B_G
-        if(i==0)
-        assign grey[Addresswidth] = binary [ Addresswidth];
-        else
-        assign grey[Addresswidth-i] = grey[Addresswidth-i+1] ^ binary[Addresswidth-i];
-        end
-endgenerate
+assign grey = binary ^ (binary >> 1); 
 endmodule 
 
