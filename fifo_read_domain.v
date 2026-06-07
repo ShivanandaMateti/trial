b971cc2f78b8b77b80cb7cba748d11873de4a7fb
ShivanandaMateti <shivamateti60@gmail.com>
2026-06-07 19:23:02 +0530

    finished_read_domain

--- fifo_read_domain.v
+++ fifo_read_domain.v
@@ -1,5 +1,5 @@
 parameter Datawidth = 8;
-parameter Addresswidth = 4;
+parameter Addresswidth = 3;
 
 module fifo_read_domain(
                             input [Addresswidth:0] write_ptr_grey_sync,
@@ -9,42 +9,49 @@ module fifo_read_domain(
                             output [Addresswidth-1:0] read_address,
                             output [Addresswidth:0]   read_ptr_grey,
                             output empty
-)
+);
 
-wire [Addresswidth:0]read_ptr;
-reg [Addresswidth:0]read_address_reg;
+reg [Addresswidth:0]read_ptr;
 wire [Addresswidth:0]read_ptr_next;
 wire [Addresswidth:0]read_ptr_grey_next;
+wire read_inc;
+assign read_inc = read_enable & ~empty;
+assign read_ptr_next = read_ptr + read_inc;
 
 always@(posedge read_clk,posedge read_reset)
 begin
     if(read_reset)
-    begin
         read_ptr <= 0;
-        read_address_reg <= 0;
-    end
-    else if (read_enable && !empty)
-    begin
-        read_ptr <= read_ptr + 1;
-        read_address_reg <= read_ptr[Addresswidth-1:0];
-    end
+    else 
+        read_ptr <= read_ptr_next;
 end
 
-assign read_address = read_address_reg;
-assign read_ptr_next = read_ptr + 1;
+assign read_address = read_ptr[Addresswidth-1:0];
 
-B_G  R1(read_ptr,read_ptr_grey);
-B_G  R1(read_ptr_next,read_ptr_grey_next);
+B_G  #(Addresswidth)R1(read_ptr,read_ptr_grey);
+B_G  #(Addresswidth)R2(read_ptr_next,read_ptr_grey_next);
 
 
-assign empty = (read_ptr_grey_next  == write_ptr_grey_sync);
+reg empty_reg;
+
+always @(posedge read_clk or posedge read_reset)
+begin
+    if (read_reset)
+        empty_reg <= 1'b1;
+    else
+        empty_reg <= (read_ptr_grey_next == write_ptr_grey_sync);
+end
+
+assign empty = empty_reg;
 
 endmodule
 
 
 
 
-module B_G(input [Addresswidth:0]binary,output [ Addresswidth:0]grey);
+module B_G#(parameter Addresswidth = 3)
+           (input [Addresswidth:0]binary,
+            output [ Addresswidth:0]grey);
 assign grey = binary ^ (binary >> 1); 
 endmodule 
 
