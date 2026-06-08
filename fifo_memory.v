757e3a8f0c7eadf92584ea2f5c724684e7dc595d
ShivanandaMateti <shivamateti60@gmail.com>
2026-06-08 17:33:52 +0530

    modified local memory also

--- fifo_memory.v
+++ fifo_memory.v
@@ -4,7 +4,9 @@ module fifo_memory #(
                     )
                     (
                         input write_clk,
+                        input read_clk,
                         input write_enable,
+                        input read_enable,
                         input [Datawidth-1:0]write_data,
                         input [Addresswidth-1:0]write_address,
                         input [Addresswidth-1:0]read_address,
@@ -17,8 +19,14 @@ localparam depth = 1 << Addresswidth;
 
 
 reg [Datawidth-1:0] memory [0 : depth-1];
+reg [Datawidth-1:0] read_data_reg;
+always@(posedge read_clk)
+    begin
+        if(read_enable)
+            read_data_reg = memory[read_address];
+    end
 
-assign read_data = memory[read_address];
+assign read_data = read_data_reg;
 
 always@(posedge write_clk)
     begin
